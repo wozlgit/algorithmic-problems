817cbcf290da1461bb13045e11d64c58cf95cc9e
Oula <oulaliim@gmail.com>
2022-06-01 14:37:39 +0300

    'Check if a string contains all binary codes of size k': Fix not counting the first binary number that was found, Use stack-allocated arrays instead of heap-allocated, Remove counting the length of the given str at the start and instead check whether we're accessing memory out of the bounds of the string on the fly

--- Leetcode/check-if-a-string-contains-all-binary-codes-of-size-k/main.c
+++ Leetcode/check-if-a-string-contains-all-binary-codes-of-size-k/main.c
@@ -2,63 +2,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 bool hasAllCodes(char * s, int k) {
-    uint8_t *binaryCodesFound;
-    uint32_t binaryCodesFoundCount, strLength, binaryNumber, multiplier;
-    
+    if(s == NULL) return false;
     const uint32_t requiredBinaryCodeCount = 1U << k;
+
+    uint8_t binaryCodesFound[requiredBinaryCodeCount];
+    uint32_t binaryCodesFoundCount, binaryNumber, multiplier;
     
-    for(strLength = 0; s[strLength] != '\0'; strLength++);
-    // strLength - k + 1 will be the number of distinct binary numbers of size k
-    // that can fit in a string of length strLength.
-    if(strLength - k + 1 < requiredBinaryCodeCount) return false;
-    
-    binaryCodesFound = (uint8_t *) malloc(sizeof(uint8_t) * requiredBinaryCodeCount);
     for(uint32_t i = 0; i < requiredBinaryCodeCount; i++) binaryCodesFound[i] = 0;
-    
+
     // Get the initial binary number
     multiplier = 1;
     binaryNumber = 0;
     for(uint32_t i = 0; i < k; i++) {
+        if(s[i] == '\0') return false;
         s[i] = s[i] - '0';
         binaryNumber += s[i] * multiplier;
         multiplier *= 2;
     }
     binaryCodesFound[binaryNumber] = 1;
+    binaryCodesFoundCount = 1;
     //multiplier was multiplied by 2 one extra time, so we have to undo that such
     //that we can use multiplier later on.
     multiplier /= 2;
     // Get all the other numbers
-    for(uint32_t i = k; i < strLength; i++) {
+    for(uint32_t i = k; s[i] != '\0'; i++) {
         // Convert s[i] from a character to a digit
         s[i] = s[i] - '0';
         // Perform a right bit-shift by 1 on the previous number (divide it by 2):
         // this will remove the first digit in the number and shift the multipliers of
         // all other digits by one. Then add the last digit of this number in.
         binaryNumber = (binaryNumber >> 1) + s[i] * multiplier;
+
+        // If the current binary number has already been found 
+        // (if binaryCodesFound[binaryNumber] is 1), then dont increment binaryCodesFoundCount
+        binaryCodesFoundCount += 1 - binaryCodesFound[binaryNumber];
+        if(binaryCodesFoundCount >= requiredBinaryCodeCount) return true;
         
         binaryCodesFound[binaryNumber] = 1;
     }
-    for(uint32_t i = 0; i < requiredBinaryCodeCount; i++) {
-        binaryCodesFoundCount += binaryCodesFound[i];
-    }
-    free(binaryCodesFound);
-    if(binaryCodesFoundCount >= requiredBinaryCodeCount) return true;
-    else return false;
+    return false;
 }
 
 int main() {
-    char *s = (char *) malloc(sizeof(char) * 8);
-    s[0] = '0';
-    s[1] = '0';
-    s[2] = '1';
-    s[3] = '1';
-    s[4] = '0';
-    s[5] = '1';
-    s[6] = '1';
-    s[7] = '0';
+    char s[] = "00110110\0";
     printf("%d\n", hasAllCodes(s, 2));
-    free(s);
     return 0;
 }
