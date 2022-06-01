26767274541153be44e546b0a4420acdfac73ce5
Oula <oulaliim@gmail.com>
2022-06-01 15:23:13 +0300

    Remove the multiplier variable and use bit-shifting to the left instead, Integrate the calculation of the initial number into the loop that calculates the other numbers too

--- Leetcode/check-if-a-string-contains-all-binary-codes-of-size-k/main.c
+++ Leetcode/check-if-a-string-contains-all-binary-codes-of-size-k/main.c
@@ -7,40 +7,37 @@ bool hasAllCodes(char * s, int k) {
     const uint32_t requiredBinaryCodeCount = 1U << k;
 
     uint8_t binaryCodesFound[requiredBinaryCodeCount];
-    uint32_t binaryCodesFoundCount, binaryNumber, multiplier;
+    uint32_t binaryCodesFoundCount, binaryNumber;
     
     for(uint32_t i = 0; i < requiredBinaryCodeCount; i++) binaryCodesFound[i] = 0;
 
-    // Get the initial binary number
-    multiplier = 1;
     binaryNumber = 0;
-    for(uint32_t i = 0; i < k; i++) {
-        if(s[i] == '\0') return false;
-        s[i] = s[i] - '0';
-        binaryNumber += s[i] * multiplier;
-        multiplier *= 2;
-    }
-    binaryCodesFound[binaryNumber] = 1;
-    binaryCodesFoundCount = 1;
-    //multiplier was multiplied by 2 one extra time, so we have to undo that such
-    //that we can use multiplier later on.
-    multiplier /= 2;
-    // Get all the other numbers
-    for(uint32_t i = k; s[i] != '\0'; i++) {
+    binaryCodesFoundCount = 0;
+    for(uint32_t i = 0; s[i] != '\0'; i++) {
         // Convert s[i] from a character to a digit
         s[i] = s[i] - '0';
-        // Perform a right bit-shift by 1 on the previous number (divide it by 2):
-        // this will remove the first digit in the number and shift the multipliers of
-        // all other digits by one. Then add the last digit of this number in.
-        binaryNumber = (binaryNumber >> 1) + s[i] * multiplier;
+        if(i < k) {
+            // Get the initial number
+            binaryNumber += (s[i] << i);
+        }
+        else {
+            // If the previous binary number has already been found
+            // (if binaryCodesFound[binaryNumber] is 1), then dont increment binaryCodesFoundCount
+            binaryCodesFoundCount += 1 - binaryCodesFound[binaryNumber];
+            if(binaryCodesFoundCount >= requiredBinaryCodeCount) return true;
+            
+            binaryCodesFound[binaryNumber] = 1;
 
-        // If the current binary number has already been found 
-        // (if binaryCodesFound[binaryNumber] is 1), then dont increment binaryCodesFoundCount
-        binaryCodesFoundCount += 1 - binaryCodesFound[binaryNumber];
-        if(binaryCodesFoundCount >= requiredBinaryCodeCount) return true;
-        
-        binaryCodesFound[binaryNumber] = 1;
+            // Perform a right bit-shift by 1 on the previous number (divide it by 2):
+            // this will remove the first digit in the number and shift the multipliers of
+            // all other digits by one. Then add the last digit of this number in (the last
+            // digit is bit-shifted to the left by k - 1 because it is the last digit in the
+            // binary number of size k and thus the multiplier is 2^(k -1)).
+            binaryNumber = (binaryNumber >> 1) + (s[i] << (k - 1));
+        }
     }
+    binaryCodesFoundCount += 1 - binaryCodesFound[binaryNumber];
+    if(binaryCodesFoundCount >= requiredBinaryCodeCount) return true;
     return false;
 }
 
